d6aa6a80bc7b8de3582a668cf5c5e5d4775ac61b
TistaMilani <francesco.rocchiccioli.02@gmail.com>
2025-11-14 15:09:19 +0100

    Basic classes utilities

--- framework/utils.py
+++ framework/utils.py
@@ -1,10 +1,7 @@
 from dataclasses import dataclass
 from typing import List
 from tree_sitter import Point, Node
-import pickle
 
-global MAP_PATH
-MAP_PATH = "framework/aaa_map.pkl"
 
 @dataclass
 class Parameter:
@@ -16,31 +13,20 @@ class Parameter:
         self.type = type
 
 @dataclass
-class AssertionNodeInfo:
-    start_byte: int
-    end_byte: int
-    source_code: str
-    
-    def __init__(self, start_byte: int, end_byte: int, source_code: str):
-        self.start_byte = start_byte
-        self.end_byte = end_byte
-        self.source_code = source_code
-
-@dataclass
 class Assertion:
     absolute_start_line: Point
     absolute_end_line: Point
-    assert_ast: AssertionNodeInfo
+    assert_ast: Node
     classification: str 
     
-    def __init__(self, absolute_start_line: Point, absolute_end_line: Point, assert_ast: AssertionNodeInfo, classification: str):
+    def __init__(self, absolute_start_line: Point, absolute_end_line: Point, assert_node: Node, classification: str):
         self.absolute_start_line = absolute_start_line
         self.absolute_end_line = absolute_end_line
-        self.assert_ast = assert_ast
+        self.assert_node = assert_node
         self.classification = classification
 
 @dataclass
-class AAAMethod:
+class Methods:
     method_name: str
     parameters: List[Parameter]
     assertions: List[Assertion]
@@ -52,56 +38,52 @@ class AAAMethod:
 
 
 @dataclass
-class AAAClass:
+class Classes:
     class_name: str
-    methods: List[AAAMethod]
+    methods: List[Methods]
     
-    def __init__(self, class_name: str, methods: List[AAAMethod]):
+    def __init__(self, class_name: str):
         self.class_name = class_name
-        self.methods = methods
+        self.methods = []
+
+    def add_method(self, method: Methods):
+        self.methods.append(method)
+        
+    def return_method(self, method_name: str) -> Methods:
+        for method in self.methods:
+            if method.method_name == method_name:
+                return method
+        return None
+    
+    def method_present(self, method_name: str) -> bool:
+        for method in self.methods:
+            if method.method_name == method_name:
+                return True
+        return False
 
 @dataclass
-class AAAMap:
-    classes: List[AAAClass]
+class Map:
+    classes: List[Classes]
     
-    def __init__(self, classes: List[AAAClass]):
-        self.classes = classes
+    def __init__(self):
+        self.classes = []
 
-    def save_to_file(self, filepath: str) -> None:
-        with open(filepath, 'wb') as f:
-            pickle.dump(self, f)
-
-    @staticmethod
-    def load_from_file(filepath: str) -> "AAAMap":
-        with open(filepath, 'rb') as f:
-            return pickle.load(f)
-        
-
-
-
-'''
-{
-  "class_name": {
-    "method_id": {
-      "parameters" : [
-        {
-          "name" : "parameter_name",
-          "type" : "parameter_type"
-        },
-        "..." // other parameters
-      ],
-      "assertions" : [
-        {
-          "absolute_start_line" : Point,
-          "absolute_end_line" : Point,
-          "assert_": "{custom_assertion_type}",
-          "classification": "undefined | side_effects | tautological | incorrect | logically_implied | useless | useful"
-        },
-        "..." // other assertions
-      ]
-    },
-    "..." // other methods
-  },
-  "..." // other classes
-}
-'''
+    def add_class(self, class_name: str):
+        self.classes.append(Classes(class_name))
+    
+    def return_class(self, class_name: str) -> Classes:
+        for cls in self.classes:
+            if cls.class_name == class_name:
+                return cls
+        return None
+    
+    def add_method_to_class(self, class_name: str, method: Methods):
+        cls = self.return_class(class_name)
+        if cls:
+            cls.add_method(method)
+    
+    def class_present(self, class_name: str) -> bool:
+        for cls in self.classes:
+            if cls.class_name == class_name:
+                return True
+        return False
